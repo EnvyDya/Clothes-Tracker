4fe88005750b49dcaa15fac1c9548c9a5e1930e3
EnvyDya <bonduelleclment@gmail.com>
2025-10-31 21:52:33 +0100

    Refacto and comments

--- src/wip/get_weather_informations.py
+++ src/wip/get_weather_informations.py
@@ -1,46 +1,86 @@
-from datetime import datetime
-from dotenv import load_dotenv
+"""Module that handles API calls to Meteosource.
+Possibility of getting places IDs or local weather of the day.
+
+Raises:
+    geocoding.raise_for_status:
+        Exception raised if any error code from the API during the geocoding.
+    weather.raise_for_status: 
+        Exception raised if any error code from the API during weather retrieval.
+
+Returns:
+    _type_: 0 if everything went correctly
+"""
 import os
-import re
+import sys
+from dotenv import load_dotenv
 import requests
 
 # Load env var from .env file
 load_dotenv()
+METEOSOURCE_API_KEY = os.getenv("METEOSOURCE_API_KEY")
 
 # Get APIs endpoints
 METEOSOURCE_API_ENDPOINT = 'https://www.meteosource.com/api/v1/free/'
 FIND_PLACE_API = 'find_places'
 WEATHER_API = 'point'
 
-if __name__ == "__main__":
-    # Recover API Key from environment variable
-    meteosource_api_key = os.getenv("METEOSOURCE_API_KEY")
+def get_place_id(place:str) -> str:
+    """Uses Meteosource API's to retrieve the place identifier.
 
-    # Printing prediction date
-    today = datetime.today()
-    print(f"Predicting for the date of {today.date()}.")
+    Args:
+        place (str): Name of the place you're in.
 
-    # Asking localization to user
-    city_name = input("Where do you live ? ") # TODO: Prevent injections in API request
-    params_geocoding = {
-        'text': city_name,
-        'key': meteosource_api_key,
-    }
-    geocoding = requests.get(METEOSOURCE_API_ENDPOINT+FIND_PLACE_API, params_geocoding)
-    if geocoding.ok:
-        city_informations = geocoding.json()[0]
-        params_weather = {
-            'place_id': city_informations["place_id"],
-            'sections': 'daily',
-            'key': meteosource_api_key,
-        }
-        weather = requests.get(METEOSOURCE_API_ENDPOINT+WEATHER_API, params_weather)
-        if weather.ok:
-            weather_informations = weather.json()['daily']['data'][0]
-            print(weather_informations)
-        else:
-            weather.raise_for_status()
-    else:
+    Raises:
+        geocoding.raise_for_status: Exception raised if any error code from the API.
+
+    Returns:
+        str: place identifier given by Meteosource API's.
+    """
+    # Request on meteosource API
+    geocoding = requests.get(
+        url = METEOSOURCE_API_ENDPOINT+FIND_PLACE_API,
+        params = {
+            'text': place,
+            'key': METEOSOURCE_API_KEY,
+        },
+        timeout=10,
+    )
+    if not geocoding.ok:
         # If response code is not ok (200), print the resulting http error code with description
-        geocoding.raise_for_status()
-        
+        raise geocoding.raise_for_status()
+    return geocoding.json()[0]["place_id"]
+
+def get_todays_weather(place_id:str) -> dict:
+    """Uses Meteosource API's to retrieve the weather of the day on a given place.
+
+    Args:
+        place_id (str): Place identifier following the Meteosource API convention.
+
+    Raises:
+        weather.raise_for_status:  Exception raised if any error code from the API.
+
+    Returns:
+        dict: All the information corresponding to the weather of the day.
+    """
+    weather = requests.get(
+        url = METEOSOURCE_API_ENDPOINT+WEATHER_API,
+        params = {
+            'place_id': place_id,
+            'sections': 'daily',
+            'key': METEOSOURCE_API_KEY,
+        },
+        timeout=10,
+    )
+    if not weather.ok:
+        # If response code is not ok (200), print the resulting http error code with description
+        raise weather.raise_for_status()
+    weather_informations = weather.json()['daily']['data'][0]
+    print(weather_informations)
+    return weather_informations
+
+
+if __name__ == "__main__":
+    # Asking localization to user
+    city_name = input("Where do you live ? ")
+    get_todays_weather(get_place_id(city_name))
+    sys.exit()
